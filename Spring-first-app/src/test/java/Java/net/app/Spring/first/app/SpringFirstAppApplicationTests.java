24894918620bb82c41b105b6b5452f3adc70c6aa
HP <HP@DESKTOP-GH9EJH5.fritz.box>
2019-12-10 12:31:07 +0100

    First Test

---
+++ Spring-first-app/src/test/java/Java/net/app/Spring/first/app/SpringFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Java.net.app.Spring.first.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
